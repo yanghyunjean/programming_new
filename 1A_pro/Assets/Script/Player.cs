1710943219a7e6b98995e4c3fc4ab0f889f1cceb
yanghyunjean <hyunjean.yang@gmail.com>
2026-05-26 16:12:10 +0900

    11

---
+++ 1A_pro/Assets/Script/Player.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 0.3f;
+
+    void Update()
+    {/*
+        // 앞으로
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+
+        // 뒤로
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+
+        // 왼쪽
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        // 오른쪽
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        */
+    }
+}
